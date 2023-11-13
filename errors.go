1b53fb12958bda4e471161b0ff818a0a99e94e2f
fuyang <fuyang108@gmail.com>
2023-11-13 17:56:36 +0800

    fix: file ReadAt offset

--- errors.go
+++ errors.go
@@ -2,14 +2,15 @@ package minio
 
 import (
 	"errors"
-	"syscall"
+	"os"
 )
 
 var (
 	ErrNoBucketInName     = errors.New("no bucket name found in the name")
-	ErrFileClosed         = errors.New("file is closed")
-	ErrOutOfRange         = errors.New("out of range")
 	ErrObjectDoesNotExist = errors.New("storage: object doesn't exist")
 	ErrEmptyObjectName    = errors.New("storage: object name is empty")
-	ErrFileNotFound       = syscall.ENOENT
+	ErrFileClosed         = os.ErrClosed
+	ErrOutOfRange         = errors.New("out of range")
+	ErrFileNotFound       = os.ErrNotExist
+	ErrNotSupported       = errors.New("doesn't support this operation")
 )
